264d491d999e37928851f663b0f08ffc2e562dfb
du00d <whjwhfwhsits@gmail.com>
2020-11-03 19:34:16 -0800

    1st

---
+++ VSCC.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+wget https://www.netlib.org/benchmark/hpl/hpl-2.3.tar.gz
+
+tar -xvf hpl-2.3.tar.gz
+rm hpl-2.3.tar.gz
+cp Make.Linux_AMD_BLIS ./hpl-2.3
+cd ./hpl-2.3
+make arch=Linux_AMD_BLIS
+
