af9329d0f92d57034f65419e58d4273846995620
du00d <whjwhfwhsits@gmail.com>
2020-11-04 21:53:25 -0800

    update

--- VSCC.sh
+++ VSCC.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "INSTALLATION BEGINS"
+echo "####### HPL #######"
 
 wget https://www.netlib.org/benchmark/hpl/hpl-2.3.tar.gz
 
@@ -8,4 +10,7 @@ rm hpl-2.3.tar.gz
 cp Make.Linux_AMD_BLIS ./hpl-2.3
 cd ./hpl-2.3
 make arch=Linux_AMD_BLIS
+
+echo "HPL DONE"
+
 
